1ec2211a5a77d36a031787ef672f684f3bfd4f6b
martin <martin.hermant@gmail.com>
2014-05-23 18:31:33 +0200

    dfsfsdsfsdfs

--- src/algorithms/giantSteps/SuperFluxPeaks.h
+++ src/algorithms/giantSteps/SuperFluxPeaks.h
@@ -128,7 +128,7 @@ _algo->configure(this->_params);
 int aqS = _algo->parameter("frameRate").toReal() * max(_algo->parameter("pre_avg").toInt(),_algo->parameter("pre_max").toInt()) / 1000;
     EXEC_DEBUG("setAcquireSize" << aqS);
     _signal.setAcquireSize(aqS);
-    _signal.setReleaseSize(aqS);
+    _signal.setReleaseSize(1);
     _peaks.setAcquireSize(1);
     _peaks.setReleaseSize(1);
     _rawmode = _algo->parameter("rawmode").toBool();
